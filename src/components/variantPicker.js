09bd218d9662c8e66b3c2491ce56426777c561ac
Priyanka Thakur <priyankathakur221206@gmail.com>
2022-02-25 16:20:15 +0530

    added styling on variant picker

--- src/components/variantPicker.js
+++ src/components/variantPicker.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faIndianRupeeSign } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import CurrencyCurrent from "./currencyCurrent";
-let select='false';
 
 function VariantPicker(props) {
-  const val=props.val;
+
+  const[select,setselect]=useState(false);
+ 
+  function clickUpdate(){
+    setselect(!select);
+  }
+
   return (
-    <div className="flex items-center -mr-4 pb-6" onClick={()=>(!select)}>
-      {
-        console.log(select)
-      }
+    <div className="flex items-center -mr-4 pb-6" onClick={clickUpdate}>
   <div className={`rounded-lg p-3 font-sans text-sm font-semibold w-11/12  
-  ${val?"bg-black_sh1 border-white border-2":"bg-black_sh"}`}>
+  ${select?"bg-black_sh1 border-white border-2":"bg-black_sh"}`}>
     
-      <div className={`bg-blue-600 rounded-xl flex text-xxs p-1 px-2 justify-center w-fit gap-1 ${val?'hidden':'block'}`}>
+      <div className={`bg-blue-600 rounded-xl flex text-xxs p-1 px-2 justify-center w-fit gap-1 ${props.val?'hidden':'block'}`}>
         <FontAwesomeIcon icon={faStar} className="h-1.5 mt-0.5" />
         <div>MOST POPULAR</div>
         <FontAwesomeIcon icon={faStar} className="h-1.5 mt-0.5" />
@@ -24,7 +26,7 @@ function VariantPicker(props) {
       <div className="text-lg font-custfont font-semibold mt-1">
         {props.count[0]} Months Pack ({props.count[1]} tubes)
       </div>
-      <div className={`flex justify-between text-xs p-1 ${val?"text-back_blue":" text-slate-500"}`}>
+      <div className={`flex justify-between text-xs p-1 ${select?"text-back_blue":" text-slate-500"}`}>
         <div>
           Savings :
           <FontAwesomeIcon icon={faIndianRupeeSign} />
@@ -32,16 +34,16 @@ function VariantPicker(props) {
         </div>
         <div className="-mt-3 font-bold text-base">
           <CurrencyCurrent text={props.count[2]} color={false} strike={false} />
-          <CurrencyCurrent text={props.count[3]} color={true} strike={true} className={`${val?" text-black_sh":"text-slate-500"}`}/>
+          <CurrencyCurrent text={props.count[3]} color={true} strike={true} className={`${props.val?" text-black_sh":"text-slate-500"}`}/>
         </div>
       </div>
       <div className="flex gap-6 text-green-600 -mt-1">
         12% Saved 
-        <div className={`text-yellow_sh ${val?'hidden':'block'}`}>Best Result</div>
+        <div className={`text-yellow_sh ${props.val?'hidden':'block'}`}>Best Result</div>
       </div>
     </div>
 
-    <div className={`${val?'block':'hidden'}`}><FontAwesomeIcon icon={faCheck} className="h-6 w-6 ml-3"/></div>
+    <div className={`${select?'block':'hidden'}`}><FontAwesomeIcon icon={faCheck} className="h-6 w-6 ml-3"/></div>
     </div>
   );
 }
